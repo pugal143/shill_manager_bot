921bf4589f8c293d987e49866a0a6f7de100171c
pugal143 <85311961+pugal143@users.noreply.github.com>
2022-04-06 15:45:03 +0530

    Update admin_fun.py

--- admin_fun.py
+++ admin_fun.py
@@ -7,11 +7,12 @@ commands_list = ["hol_user_list", "hol_user_remove", "remove_admin", "add_admin"
                  "commands_list", "del_event", "set_new_event", "set_new_form", "add_qn", "hol_user_add",
                  "request_question", "permission_list", "announcement_user", "give_all_questions"]
 
-MONGODB_URI = "mongodb+srv://pugalkmc:pugalkmc@cluster0.vx30p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+MONGODB_URL = "mongodb+srv://pugalkmc:pugalkmc@cluster0.vx30p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-client = pymongo.MongoClient(MONGODB_URI)
+# MONGODB_URI = os.environ["mongodb+srv://pugalkmc:pugalkmc@cluster0.vx30p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"]
 
-mydb = client["botdbs"]
+client = MongoClient(MONGODB_URL)
+mydb = client.get_default_database()
 
 def admin_mod(update, text, bot, telegram):
     username = update.message.chat.username
