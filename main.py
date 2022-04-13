94e03a67db152bb3b49a5555dd1d8bf60dc6ed0c
pugal143 <85311961+pugal143@users.noreply.github.com>
2022-04-13 14:43:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ client = MongoClient(MONGODB_URL)
 mydb = client.get_default_database()
 
 
-def start(update , context):
+def tart(update , context):
     sender = update.message.reply_text
     chat_id = update.message.chat_id
     username = update.message.chat.username
